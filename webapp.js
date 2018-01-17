28cd9a3cd098157f765bea7d3b07df7b29f9278b
Joshua Huang <joshua.huang@warriorlife.net>
2018-01-17 11:50:55 -0800

    1st Day
    Finished readme
    Added buttons and css

--- webapp.js
+++ webapp.js
@@ -1,1 +1,11 @@
 // JavaScript Document
+function startgame() {
+	alert("Welcome to Jumanji");
+	var name = prompt("What is your name?");
+	if (name == null || name == " ") {
+		
+	}
+	else {
+		alert("Hello," + name);	
+	}
+}
