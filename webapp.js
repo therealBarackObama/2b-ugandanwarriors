2983c6bea204b7d3a50fbfa0c21f5e844783521e
Joshua Huang <joshua.huang@warriorlife.net>
2018-01-19 10:47:25 -0800

    Reverted Changesq

--- webapp.js
+++ webapp.js
@@ -1,11 +1,1 @@
 // JavaScript Document
-function startgame() {
-	alert("Welcome to Jumanji");
-	var name = prompt("What is your name?");
-	if (name == null || name == " ") {
-		
-	}
-	else {
-		alert("Hello," + name);	
-	}
-}
