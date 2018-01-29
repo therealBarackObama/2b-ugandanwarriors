8cd004ca87deae9bcb6268fc67d821f3a6e04664
Joshua Huang <joshua.huang@warriorlife.net>
2018-01-29 11:27:50 -0800

    Stats and Weapon Page

--- webapp.js
+++ webapp.js
@@ -1,1 +1,5 @@
 // JavaScript Document
+function intro() {
+	confirm("By starting a new game, you will replace any old saves. Do you wish to proceed?")
+	var playername = prompt("What is your name?");	
+}
