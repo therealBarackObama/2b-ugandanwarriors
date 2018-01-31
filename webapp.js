8b7ec4fb1b88ab36e042108d89b1bfdca7befb71
Joshua Huang <joshua.huang@warriorlife.net>
2018-01-31 11:57:33 -0800

    Basic Stats Addition

--- webapp.js
+++ webapp.js
@@ -1,5 +1,6 @@
 // JavaScript Document
 function intro() {
 	confirm("By starting a new game, you will reaplce any old saves. Do you wish to proceed?");
-	var playername = prompt("What is your name?");	
+	var playername = prompt("What is your name?");
+	document.getElementById("stats_name").innerHTML = "Name: " + playername;
 }
