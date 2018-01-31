1c2e4f26b96cdb1349739aa6665cd338ad95e54f
Joshua Huang <joshua.huang@warriorlife.net>
2018-01-31 11:14:25 -0800

    Basic Template

--- webapp.js
+++ webapp.js
@@ -1,9 +1,5 @@
 // JavaScript Document
 function intro() {
-<<<<<<< HEAD
 	confirm("By starting a new game, you will reaplce any old saves. Do you wish to proceed?");
-=======
-	confirm("By starting a new game, you will replace any old saves. Do you wish to proceed?")
->>>>>>> 8cd004ca87deae9bcb6268fc67d821f3a6e04664
 	var playername = prompt("What is your name?");	
 }
